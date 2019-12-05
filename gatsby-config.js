a78f0ddc9b9a81c5253daf4da37fdf0601039a58
Ian Homer <ian.homer@gmail.com>
2019-12-05 22:09:15 +0000

    Kick off

---
+++ gatsby-config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  siteMetadata: {
+    title: `Collective`,
+    description: `Collective Content`,
+    author: `Ian`
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-thingity`,
+    {
+      options: {
+        name: `content`,
+        path: `${__dirname}/content`
+      },
+      resolve: `gatsby-source-filesystem`,
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-typescript`,
+  ]
+}
